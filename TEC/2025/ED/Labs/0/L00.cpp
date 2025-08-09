d325ad2c7c0f28ab4a0d69a5a83e27a10a9eaaff
Conoutlet <benjaoq@gmail.com>
2025-08-09 16:31:40 -0600

    fix: improve square root function and enhance code comments for clarity

--- TEC/2025/ED/Labs/0/L00.cpp
+++ TEC/2025/ED/Labs/0/L00.cpp
@@ -1,21 +1,45 @@
 //Hola , este es mi primer programa en C++. espero no morir en el intento :p  este es el ejercicio de los bolos xddd. 
 //Se tiene primero el mod 
 
+
+
+// sEFINIR TIPO DE DATOS 
+
+//IFS VA DENTRO DE PARENTESIS 
+//USA LLAVES {} COMO IDENTACION 
+//  >> FLECHAS DE DIRECCION PRA SALIDAS Y ENTRADAS << , COMO UN IMP UT EL COU/CIN
+// AND = && 
+//OR == ||
+//NOT = !
+// <>
+//MATCH = IF ELIF PERO EFICIENTE 
+//FORS SON 3 STATEMENTS, DECLARA EL CONTADOR, CONDICION , INCREMENTADOR 
+// 
+
+#include <iostream>
+using namespace std;
 int mod = 1000000007;
 
 //Raiz cuadrada de 
-long double raiz (int x) {
-    long double er, inf, sup, m;
+long double raiz (int n) {
+    long double er, inf, sup, m,x ;
     er= 0.00001;
     inf = 1;
-    sup = m;
+    sup = n;
+    x=n;
     while (sup-inf > er) {
         m = (inf + sup) / 2;
         if (m * m < x) {
             inf = m;
         } else {
-            inf = m;
+            sup = m;
         }
     }
     return m;
+}
+
+int main () {
+    int n ;
+    cin >> n ;
+    cout << raiz(n) << endl;
 }
