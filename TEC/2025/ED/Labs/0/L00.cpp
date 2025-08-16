ddb40f9b90ff32c9954c1a617f74033bb4138b23
Conoutlet <benjaoq@gmail.com>
2025-08-16 17:36:10 -0600

    feat: enhance list implementation with insertion and search methods

--- TEC/2025/ED/Labs/0/L00.cpp
+++ TEC/2025/ED/Labs/0/L00.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 using namespace std;
-//Raiz cuadrada de 
 int  sum_bolos_niv (int n) {
-    long double inf, sup, m,x ;
-    inf = 0;
-    sup = n;
-    x=n;
+    long double inf, sup, m;
+    inf = 0; sup = n;
     while (sup-inf > 0.000001) {          //este es el algoritmo de busqueda biaria. si es que se hiciese con formua directa, el tiempo seraia exponencial no cuadrado, por un elevado al cubo.
         m = (inf + sup) / 2;
-        if ((m * (m+1))/2 < x) {
+        if ((m * (m+1))/2 < n) {
             inf = m;
         } else {
             sup = m;
@@ -24,25 +21,6 @@ int main () {
     cin >> entrada ;
     int lista [entrada];
     for (int i=0 ; i<entrada ; i++) {int c_p;cin >> c_p;lista[i] = c_p;}
-    for (int j=0 ; j<entrada ; j++) {
-        cout << sum_bolos_niv(lista[j]) << endl;
+    for (int j=0 ; j<entrada ; j++) {cout << sum_bolos_niv(lista[j]) << endl;
     }
 }
-//Hola , este es mi primer programa en C++. espero no morir en el intento :p  este es el ejercicio de los bolos xddd. 
-//Se tiene primero el mod 
-
-
-
-// DEFINIR TIPO DE DATOS 
-//IFS VA DENTRO DE PARENTESIS 
-//USA LLAVES {} COMO IDENTACION 
-//  >> FLECHAS DE DIRECCION PRA SALIDAS Y ENTRADAS << , COMO UN IMP UT EL COU/CIN
-// AND = && 
-//OR == ||
-//NOT = !
-// <>
-//MATCH = IF ELIF PERO EFICIENTE 
-//FORS SON 3 STATEMENTS, DECLARA EL CONTADOR, CONDICION , INCREMENTADOR 
-// MAIN TIRA 0 POR DEFEFCTO ]
-// N= ; N ES EL PUNTERO A ESE VALOR EN MEMORIA , LITERALMENTE ES COMO DEFINIR UNA VARIABLE. 
-
--- TEC/2025/ED/Labs/0/L02.cpp
+++ TEC/2025/ED/Labs/0/L02.cpp
@@ -1,10 +1,39 @@
-/*      Batalla Olvidada de Hormigas        */
-//Parejas xddd
-int main (){
-    int m,n;
-    cin >> m >> n;
-    for (int i = 0; i < m; i++) {
-        
+struct Node {int valor ;Node *next; Node (int v ): valor(v), next (nullptr){}};
+class lista {
+    Node *head;
+public:
+    lista(){head=nullptr; } //empeiza aputnando a nada cuando se crea la lsita 
+    void insertar(int valor ); 
+    void imprimir ();
+}
+void lista ::insertar  (int val) { 
+    if (head == nullptr) {
+        head=new Node(val);
+    } else {
+        Node *temp = head;
+        while (temp->next) {
+            temp = temp->next;
+        }
+        temp->next = new Node(val);
     }
-
+}
+bool lista::isin(int valor) {
+    Node *temp = head;
+    while (temp) {
+        if (temp->valor == valor) {
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+int main (){
+    int m, n;
+    cin >> m >> n;
+    lista miLista;
+    for (int i = 0; i < m; i++) {
+        int i,j;
+        cin >> i >> j;
+        if (miLista.isin(i) )
+    }
 }
