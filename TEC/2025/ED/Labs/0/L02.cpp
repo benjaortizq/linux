43b6786c05e45199cbf60b4e5cfa2faa2de2c437
Conoutlet <benjaoq@gmail.com>
2025-08-18 02:04:39 -0600

    fix: correct indentation and improve clarity in node initialization and incidence creation logic

--- TEC/2025/ED/Labs/0/L02.cpp
+++ TEC/2025/ED/Labs/0/L02.cpp
@@ -1,45 +1,45 @@
-#include <iostream>
-using namespace std;
-class Node  {public : int id ;int indegree; int* from;};
-int main (){
-    int m , n ;
-    cin >> n >> m;
-    int perm[n];
-    Node* nodoArray= new Node [n]; ''' Aqui es donde empieza la incializacion de nodos apenas '''
-    for (int i = 0; i < n; i++) 
-        {  
-        nodoArray[i].id = -1;
-        nodoArray[i].indegree = 0;
-        nodoArray[i].from = new int[n-1];
-        for (int j = 0; j < n-1; j++) 
-        {
-            nodoArray[i].from[j] = -1;      // inicializar todos a -1
+    #include <iostream>
+    using namespace std;
+    class Node  {public : int id ;int indegree; int* from;};
+    int main (){
+        int m , n ;
+        cin >> n >> m;
+        int perm[n];
+        Node* nodoArray= new Node [n]; ''' Aqui es donde empieza la incializacion de nodos apenas '''
+        for (int i = 0; i < n; i++) 
+            {  
+            nodoArray[i].id = -1;
+            nodoArray[i].indegree = 0;
+            nodoArray[i].from = new int[n-1];
+            for (int j = 0; j < n-1; j++) 
+            {
+                nodoArray[i].from[j] = -1;      // inicializar todos a -1
+            }
         }
-    }
-    for (int i=0 ; i<m ; i++ ){        //ESTE ES EL FOR QUE CREA LAS INCIDENCIAS DE CADA NUMERO Y LAS GUARDA EN UNA LISTA DE INCIDENCIAS.
-        int a , b;
-        cin >> a >> b;
-        int c = 0;
-        bool encontrado = false;
-        while (c < n && !encontrado)  //recorre el nodo array hasta que encuentre el nodo o se acabe el array
-        {
-            if (nodoArray[c].id == b) {
-                encontrado = true;
-                for (int j = 0; j < n-1; j++) {
-                    if (nodoArray[c].from[j] == a) { //verifica si el nodo ya existe y si no se ha agregado el nodo A
-                        break;
-                    }
-                    if (nodoArray[c].from[j] == -1) { //evita falsos positivos, lo agrega solo si no hay A y es -1 
-                        nodoArray[c].from[j] = a;
-                        nodoArray[c].indegree++;
-                        break;
+        for (int i=0 ; i<m ; i++ ){        //ESTE ES EL FOR QUE CREA LAS INCIDENCIAS DE CADA NUMERO Y LAS GUARDA EN UNA LISTA DE INCIDENCIAS.
+            int a , b;
+            cin >> a >> b;
+            int c = 0;
+            bool encontrado = false;
+            while (c < n && !encontrado)  //recorre el nodo array hasta que encuentre el nodo o se acabe el array
+            {
+                if (nodoArray[c].id == b) {
+                    encontrado = true;
+                    for (int j = 0; j < n-1; j++) {
+                        if (nodoArray[c].from[j] == a) { //verifica si el nodo ya existe y si no se ha agregado el nodo A
+                            break;
+                        }
+                        if (nodoArray[c].from[j] == -1) { //evita falsos positivos, lo agrega solo si no hay A y es -1 
+                            nodoArray[c].from[j] = a;
+                            nodoArray[c].indegree++;
+                            break;
+                        }
                     }
                 }
+                c++;
             }
-            c++;
-        }
-        if (!encontrado) {nodoArray[c].id = b; nodoArray[c].from[0] = a; nodoArray[c].indegree = 1; }
-    }   //hasta aqui esta lista la creacion de nodos y sus reduncancias, ahora toca  ordenarlos de forma lexicograficamente minima 
+            if (!encontrado) {nodoArray[c].id = b; nodoArray[c].from[0] = a; nodoArray[c].indegree = 1; }
+        }   //hasta aqui esta lista la creacion de nodos y sus reduncancias, ahora toca  ordenarlos de forma lexicograficamente minima 
 
     while True {
 
