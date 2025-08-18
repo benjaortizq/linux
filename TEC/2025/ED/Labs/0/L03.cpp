3e3b94782805143b7074378a67e3f8376df1a431
Conoutlet <benjaoq@gmail.com>
2025-08-18 17:21:25 -0600

    feat: add initial implementation of matrix size validation in L03.cpp

---
+++ TEC/2025/ED/Labs/0/L03.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+//ejercicio 4 , matriz cuadrada mas grande 
+
+int main (){
+    int F , C  ;
+    cin >> F >> C ;
+    if (F <=0 || F >=1e4|| C <=0 || C>=1e4) {
+        cout << -1 << endl ;
+        return 0 ;
+    }
+    for (int i=0 ; i< F; i++ ) {
+        
+    }
+}
