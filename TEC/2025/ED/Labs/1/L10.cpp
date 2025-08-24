f7e3f5c15b2214eefb4ccc503086ee7cf86520cf
Conoutlet <benjaoq@gmail.com>
2025-08-23 20:02:40 -0600

    refactor: rename DLL class to Vector and update constructors for clarity

--- TEC/2025/ED/Labs/1/L10.cpp
+++ TEC/2025/ED/Labs/1/L10.cpp
@@ -9,20 +9,20 @@ class Node {
     Node* prev; // Nodo anterior en la lista
 };
 
-class DLL {
+class Vector {          //para no hacer tanto despelote, class vector funciona igual que una dll , solo que se llama vector xd 
     private : 
     int  capacidad_max ;
     public :
     Node* head; // Cabeza de la lista
     Node* tail; // Cola de la lista 
     int tamano ;
-    DLL() { //creador
+    Vector() { //creador
         head = nullptr;
         tail = nullptr;
         capacidad_max = 2147483647; //capacidad por defecto
         tamano = 0;
     }
-    DLL (int cap) { //creador con capacidad
+    Vector (int cap) { //creador con capacidad
         head = nullptr;
         tail = nullptr;
         capacidad_max = cap;
@@ -36,16 +36,17 @@ class DLL {
 
 
 };
-int DLL::size() {
+
+int Vector::size() {
         return tamano;}
-int DLL::max_size(){
+int Vector::max_size(){
         return capacidad_max;}
-void DLL::resize(int n) { //cambia el tamano maximo por el de entrada si cantidad de elementos del vector es menor o igual a entrada
+void Vector::resize(int n) { //cambia el tamano maximo por el de entrada si cantidad de elementos del vector es menor o igual a entrada
     ((tamano >= n)? capacidad_max = n : 0 ); //cambia max por n si n mayor a size actual , sino no hace nada
     return ;
 }
 
-int DLL:: at(int n ){
+int Vector:: at(int n ){
     int valor = at (n);
     return valor ;
     Node* actual = head ; 
@@ -57,7 +58,7 @@ int DLL:: at(int n ){
 
 };
 
-int& DLL::operator[] (int index){ //no hay que hacer validacion de entradas, voya  ahcerlop supoiniendo que entrada es >=0 y que el tamano de la lista siempre va a ser mayor o igual al de la entrada 
+int& Vector::operator[] (int index){ //no hay que hacer validacion de entradas, voya  ahcerlop supoiniendo que entrada es >=0 y que el tamano de la lista siempre va a ser mayor o igual al de la entrada 
     int valor = at (index ) ; 
     return valor ; 
 }
@@ -66,3 +67,4 @@ int& DLL::operator[] (int index){ //no hay que hacer validacion de entradas, voy
 
 //tener en cuenta que -> es como el :: pero en punteros 
 
+//eliminacion de dll por vector . funciona igual que una dll 
