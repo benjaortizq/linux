7eb3b05bd805f90e995182f0fc81f707dab02820
Conoutlet <benjaoq@gmail.com>
2025-09-01 17:42:29 -0600

    feat: implement priority queue insertion logic in SLL class

--- TEC/2025/ED/Labs/1/L11.cpp
+++ TEC/2025/ED/Labs/1/L11.cpp
@@ -6,20 +6,55 @@ using namespace std;
 
 class Node  { 
     public: 
-    Node* sig ; 
-    int valor ; 
-    string Nombre ; 
+    Node* next ; 
+    int pr ; 
+    string name ; 
 };
 
 class SLL{
     public :
-    int tamano ;
-    Node* Head ;
+    int size ;
+    Node* head ;
 
     void insertar ( string nombre, int prio) ;
+    void pop ();
 
 };
 
-void SLL::insertar (string nombre , int prio ) {
-    
-}
+void SLL::insertar (string nombre , int prio ) { //!o(n)
+    //revisar si la prioridad de lo que quiero insertar es menor a la del actual, tons se que debo continuar. sino entonces se que va antes del actual. 
+    Node *ins = new Node; 
+    ins->pr = prio; 
+    ins->name= nombre ;
+    ins->next= nullptr;
+    //*caso 1 : lista vacia 
+    if (head==nullptr) { 
+        head = ins;
+        size ++ ;
+        return ;
+    }
+    //*caso 2 ; lista tamano>=1
+    Node*act= head ; 
+    //soy mayor al primero  : 
+    if (act ->pr < prio ){  //insertar al inicio
+        ins->next = act ; 
+        head = ins ; 
+        size++;
+        return ;
+    }
+                
+    while (act->next !=nullptr ) {// si es igual jala despues , solamente me fijo en el siguiente si existe y que sea menor y no es el ultimo elemento 
+        if (act->next->pr < prio) {  //mi prioridad es mayor a la del siguiente ?, se que es menor que el actual
+            ins->next = act->next ; 
+            act->next = ins ; 
+            size++;
+            return ; 
+        }
+        act=act->next;
+    }
+    //insertar de ultimo 
+    act ->next = ins;
+    size++;
+    return ;
+
+
